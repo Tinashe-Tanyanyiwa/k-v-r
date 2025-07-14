bf7c704d6a1a86cf888dfa5440d45406e89e295d
Tinashe-Tanyanyiwa <tanyanyiwatinashe7@gmail.com>
2025-07-14 12:11:40 +0200

    buttons texts

--- src/app/HeroTwo.tsx
+++ src/app/HeroTwo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import background from "@/../public/images/herobackgroundfour.png";
 import backgroundtwo from "@/../public/images/herobackgroundfour.png";
 import buttonback from "@/../public/images/vector3.png";
-import buttonbacktwo from "@/../public/images/vector4.png";
+import buttonbacktwo from "@/../public/images/vector5.png";
 import rectangle from "@/../public/images/rectangle.png";
 import { usePathname } from "next/navigation";
 
@@ -48,10 +48,11 @@ function HeroTwo() {
           <div className="relative  px-[17px]    ">
             <div className=" flex  items-center justify-center">
               <div className="">
-                <h1 className=" h-[131px] mb-4 font-roboto font-medium text-[112px] leading-none tracking-[0.1em] text-center text-white">
-                  Fire. Flavor. Family.
+                <h1 className="h-[131px] mb-4 font-roboto font-medium text-[72px] leading-none tracking-[0.1em] text-center text-black">
+                  Fire. <span className="text-[#E2231A]">Flavor.</span>{" "}
+                  <span className="text-[#105742]">Family.</span>
                 </h1>
-                <p className=" font-roboto font-medium text-[36px] leading-[46.8px] tracking-[-0.2%] tracking-[1%] text-center text-white">
+                <p className=" font-roboto font-normal text-[24px] leading-[1] tracking-[-0.2%] text-center text-white">
                   Real food, real fire, real moments. <br /> Out here, the grill
                   crackles, the stories flow, and the flavors <br /> come alive.
                   Gather round, feed the fire, and let the good <br /> times
@@ -71,7 +72,9 @@ function HeroTwo() {
                 objectFit="cover"
                 className="absolute inset-0 z-0"
               />
-              <p className="text-white z-1 relative">Feed The Fire</p>
+              <p className="font-roboto font-normal text-[22px] leading-[1] tracking-[0] align-middle text-white z-1 relative">
+                Feed The Fire
+              </p>
             </button>
           </div>
           <div
@@ -85,16 +88,20 @@ function HeroTwo() {
                 objectFit="cover"
                 className="absolute inset-0 z-0"
               />
-              <p className="text-white z-1 relative">Give it a try</p>
+              <p className="font-roboto font-extrabold text-[22px] leading-[1] tracking-[0] align-middle z-1 relative">
+                Give it a try
+              </p>
             </button>
           </div>
         </div>
         <div className="mt-[72px]">
-          {/* <p className="h-[47px] font-roboto font-bold text-[36px] leading-[46.8px] tracking-[-0.2%] text-center text-white">
+          <p
+            className={` ${visible} h-[47px] font-roboto font-bold text-[20px] leading-[1] tracking-[-0.2%] text-center text-white`}
+          >
             Next PopUp
-          </p> */}
+          </p>
           <h1
-            className={`${visible} mt-[12px] font-roboto font-bold text-[72px] leading-[86px] tracking-[-0.5px] text-center text-white`}
+            className={`${visible} mt-[12px] font-roboto font-bold text-[40px] leading-[1] tracking-[-0.5px] text-center text-white`}
           >
             March 25
           </h1>
